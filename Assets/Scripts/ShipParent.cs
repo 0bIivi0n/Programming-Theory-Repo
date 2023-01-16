ae2e3ef3bad58acb6f73f0c9b31702452c84c4b4
0blivi0n <aubry.varen@gmail.com>
2023-01-16 14:54:59 +0100

    Parent, Allied and Enemy

---
+++ Assets/Scripts/ShipParent.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipParent : MonoBehaviour
+{
+    [SerializeField] int health = 100;
+    [SerializeField] int atk = 10;
+    [SerializeField] int shield = 100;
+    [SerializeField] int range = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
